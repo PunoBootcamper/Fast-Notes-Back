f1df1a570b11efd08ba1e00fceaf17d9138a7291
William Lupaca <wlupacat@gmail.com>
2024-12-10 17:34:47 -0500

    feat: add dotenv

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import "dotenv/config";
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log(process.env.PORT);
-
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
